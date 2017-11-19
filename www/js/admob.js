1df1e5555f790e6fe60b3d0fbe8991cee34fd470
bigreni <bigreni@gmail.com>
2017-11-19 16:06:36 -0600

    v9 fixes 6

--- www/js/admob.js
+++ www/js/admob.js
@@ -102,18 +102,15 @@ function showPlanner()
     document.getElementById('divPlanner').style.height = '100vh';
 }
 
- var permissions = cordova.plugins.permissions;
-
 function checkPermissions()
 {
+ var permissions = cordova.plugins.permissions;
  var list = [
   permissions.CAMERA,
   permissions.ACCESS_COARSE_LOCATION
 ];
 
 permissions.hasPermission(list, success, null);
-
-}
 
 function error() {
   alert('Location services is not enabled.');
@@ -122,13 +119,14 @@ function error() {
 function success( status ) {
     alert(status);
   if( !status.hasPermission ) {
-
-      permissions.requestPermissions(
+  
+    permissions.requestPermissions(
       list,
-      function (status) {
+      function(status) {
           alert('here');
-          if (!status.hasPermission) error();
+        if( !status.hasPermission ) error();
       },
       error);
     }
+   }
 }
